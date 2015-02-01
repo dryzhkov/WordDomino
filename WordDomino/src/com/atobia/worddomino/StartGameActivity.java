83590984baf65c09bd8f5dc6e6d9ee680b060d70
eliraneretzkedosha <eliranek@gmail.com>
2015-02-01 10:29:26 -0800

    adding alert to audiomanager and a delay of game

--- WordDomino/src/com/atobia/worddomino/StartGameActivity.java
+++ WordDomino/src/com/atobia/worddomino/StartGameActivity.java
@@ -1,27 +1,21 @@
 package com.atobia.worddomino;
 
 import android.app.Activity;
-import android.content.ActivityNotFoundException;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.media.AudioManager;
 import android.os.Bundle;
-import android.speech.RecognizerIntent;
-import android.speech.tts.TextToSpeech;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.atobia.worddomino.util.Configuration;
-import com.atobia.worddomino.util.SafetyNoticeDialog;
 
-import java.util.ArrayList;
-
-public class StartGameActivity extends Activity {
+public class StartGameActivity extends Activity implements DialogInterface.OnClickListener {
     private StartGame sg;
 
     @Override
@@ -30,31 +24,62 @@ public class StartGameActivity extends Activity {
         setContentView(R.layout.start_game);
         setVolumeControlStream(Configuration.GAME_AUDIO_STREAM);
 
-        //check current volume
-        AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
-        int curVol = audioManager.getStreamVolume(Configuration.GAME_AUDIO_STREAM);
+        // Get a handle on the input text box on the StartGameActivity page
+        EditText playOverText = (EditText)this.findViewById(R.id.play_over_text);
 
-        if(curVol < Configuration.GAME_MIN_VOLUME){
-            //TODO: we need to handle this case somehow, displaying toast message for now
-            Toast.makeText(getApplicationContext(),
-                    "Game Volume is too low. ",
-                    Toast.LENGTH_SHORT).show();
+        // Delay the game if the volume is too low
+        Boolean delayGame = false;
+
+        // Determine if they are going to play over bluetooth
+        if (Configuration.PlayOverBluetooth) {
+            //check current volume
+            AudioManager audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+            int curVol = audioManager.getStreamVolume(Configuration.GAME_AUDIO_STREAM);
+
+            if(curVol < Configuration.GAME_MIN_VOLUME){
+                AlertDialog.Builder audioAD = new AlertDialog.Builder(this);
+                // Turn it down for what?!
+                audioAD.setMessage("Game volume is too low");
+                audioAD.setCancelable(false);
+                audioAD.setNeutralButton("Ok", this);
+                audioAD.show();
+
+                // Wait to start the game until they press ok
+                delayGame = true;
+            }
+
+            // Hide the text box
+            playOverText.setVisibility(View.INVISIBLE);
+        } else {
+            playOverText.setVisibility(View.VISIBLE);
         }
 
+        if (!delayGame) {
+            // Run the game, there are no issues
+            this.runGame();
+        }
+    }
+
+    private void runGame() {
         this.sg = new StartGame(this);
         this.sg.setBackgroundColor(Color.WHITE);
         LinearLayout layout = (LinearLayout)findViewById(R.id.rootLayout);
         layout.addView(this.sg);
     }
 
+
+    // Needed for the alert dialog
+    public void onClick (DialogInterface dialog, int which) {
+        // The user is aware of the issue, it's time to play!
+        this.runGame();
+
+        dialog.dismiss();
+    }
+
     @Override
     public void onBackPressed(){
         super.onBackPressed();
         this.sg.startGameLoop.shouldRun = false;
-        /*
-        Commenting out for now because of performance hit
-        this.sg.util.SaveGame(this, this.sg.game);
-        */
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
