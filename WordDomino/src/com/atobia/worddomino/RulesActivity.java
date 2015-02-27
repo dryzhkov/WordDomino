1a55503d96616a50f41c419057d135a235905ee5
eliraneretzkedosha <eliranek@gmail.com>
2015-02-26 18:31:40 -0800

    rules page

--- WordDomino/src/com/atobia/worddomino/RulesActivity.java
+++ WordDomino/src/com/atobia/worddomino/RulesActivity.java
@@ -2,6 +2,7 @@ package com.atobia.worddomino;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
@@ -11,29 +12,47 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class RulesActivity extends Activity {
+    private Animation animationSlideInLeft;
+    private int currentIndexOfRule = 0;
+    private TextView[] AllRules;
 
-    TextView RuleTextOne, RuleTextTwo, RuleTextThree, RuleTextFour, RuleTextFive, RuleTextSix;
-    Animation animationSlideInLeft;
-    TextView curSlidingText;
-    String curTextSlider = "";
+    final static int timeToSlide = 1500;
 
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rules);
-        RuleTextOne = (TextView)findViewById(R.id.rule_text_one);
-        RuleTextTwo = (TextView)findViewById(R.id.rule_text_two);
-        RuleTextThree = (TextView)findViewById(R.id.rule_text_three);
-        RuleTextFour = (TextView)findViewById(R.id.rule_text_four);
-        RuleTextFive = (TextView)findViewById(R.id.rule_text_five);
-        RuleTextSix = (TextView)findViewById(R.id.rule_text_six);
+
+        AllRules = new TextView[6];
+
+        AllRules[0] = (TextView)findViewById(R.id.rule_text_one);
+        AllRules[1] = (TextView)findViewById(R.id.rule_text_two);
+        AllRules[2] = (TextView)findViewById(R.id.rule_text_three);
+        AllRules[3] = (TextView)findViewById(R.id.rule_text_four);
+        AllRules[4] = (TextView)findViewById(R.id.rule_text_five);
+        AllRules[5] = (TextView)findViewById(R.id.rule_text_six);
 
         animationSlideInLeft = AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
-        animationSlideInLeft.setDuration(1500);
+        animationSlideInLeft.setDuration(timeToSlide);
         animationSlideInLeft.setAnimationListener(animationSlideInLeftListener);
 
-        RuleTextOne.startAnimation(animationSlideInLeft);
+        AllRules[0].startAnimation(animationSlideInLeft);
+        AllRules[0].setVisibility(View.VISIBLE);
+
+        new CountDownTimer(timeToSlide * (AllRules.length + 1), timeToSlide) {
+
+            public void onTick(long millisUntilFinished) {
+                clearAnimations();
+                if (currentIndexOfRule < AllRules.length) {
+                    AllRules[currentIndexOfRule].startAnimation(animationSlideInLeft);
+                    AllRules[currentIndexOfRule].setVisibility(View.VISIBLE);
+                    currentIndexOfRule++;
+                }
+            }
+
+            public void onFinish() {}
+        }.start();
     }
 
     @Override
@@ -43,63 +62,20 @@ public class RulesActivity extends Activity {
     }
 
     private void clearAnimations() {
-        RuleTextOne.clearAnimation();
-        RuleTextTwo.clearAnimation();
-        RuleTextThree.clearAnimation();
-        RuleTextFour.clearAnimation();
-        RuleTextFive.clearAnimation();
-        RuleTextSix.clearAnimation();
+        for (int i = 0; i < AllRules.length; i++) {
+            AllRules[i].clearAnimation();
+        }
     }
 
     AnimationListener animationSlideInLeftListener = new AnimationListener(){
 
         @Override
-        public void onAnimationEnd(Animation animation) {
-            Log.d("starting animating", "number " + curTextSlider);
-            if (curSlidingText == null) {
-                curTextSlider = "one";
-                curSlidingText = RuleTextOne;
-                curSlidingText.startAnimation(animationSlideInLeft);
-                curSlidingText.setVisibility(View.VISIBLE);
-            } else if (curSlidingText == RuleTextOne && curSlidingText.getVisibility() == View.VISIBLE) {
-                curTextSlider = "two";
-                curSlidingText.clearAnimation();
-                curSlidingText = RuleTextTwo;
-                curSlidingText.startAnimation(animationSlideInLeft);
-                curSlidingText.setVisibility(View.VISIBLE);
-            } else if (curSlidingText == RuleTextTwo && curSlidingText.getVisibility() == View.VISIBLE) {
-                curTextSlider = "three";
-                curSlidingText.clearAnimation();
-                curSlidingText = RuleTextThree;
-                curSlidingText.startAnimation(animationSlideInLeft);
-                curSlidingText.setVisibility(View.VISIBLE);
-            } else if (curSlidingText == RuleTextThree && curSlidingText.getVisibility() == View.VISIBLE) {
-                curTextSlider = "four";
-                curSlidingText.clearAnimation();
-                curSlidingText = RuleTextFour;
-                curSlidingText.startAnimation(animationSlideInLeft);
-                curSlidingText.setVisibility(View.VISIBLE);
-            } else if (curSlidingText == RuleTextFour && curSlidingText.getVisibility() == View.VISIBLE) {
-                curTextSlider = "five";
-                curSlidingText.clearAnimation();
-                curSlidingText = RuleTextFive;
-                curSlidingText.startAnimation(animationSlideInLeft);
-                curSlidingText.setVisibility(View.VISIBLE);
-            } else if (curSlidingText == RuleTextFive && curSlidingText.getVisibility() == View.VISIBLE) {
-                curTextSlider = "six";
-                curSlidingText.clearAnimation();
-                curSlidingText = RuleTextSix;
-                curSlidingText.startAnimation(animationSlideInLeft);
-                curSlidingText.setVisibility(View.VISIBLE);
-            }
-            Log.d("end animating", "number " + curTextSlider);
-        }
+        public void onAnimationEnd(Animation animation) { }
 
         @Override
-        public void onAnimationRepeat(Animation animation) {
-        }
+        public void onAnimationRepeat(Animation animation) { }
 
         @Override
-        public void onAnimationStart(Animation animation) {
-        }};
+        public void onAnimationStart(Animation animation) { }
+    };
 }
